18eb31d5dd7a5beecd2fba60ddecbb9380cc4110
Hadrien Kohl <hadrien.kohl@gmail.com>
2018-10-22 16:17:01 +0200

    Avoid wrapping ConnectorException in cache connector

--- java-vtl-connectors-utils/src/main/java/no/ssb/vtl/connectors/utils/CachedConnector.java
+++ java-vtl-connectors-utils/src/main/java/no/ssb/vtl/connectors/utils/CachedConnector.java
@@ -9,9 +9,9 @@ package no.ssb.vtl.connectors.utils;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -99,7 +99,11 @@ public abstract class CachedConnector extends ForwardingConnector {
                 };
             });
         } catch (ExecutionException e) {
-            throw new ConnectorException("cache error", e.getCause());
+            if (e.getCause() instanceof ConnectorException) {
+                throw (ConnectorException) e.getCause();
+            } else {
+                throw new ConnectorException("cache error", e.getCause());
+            }
         }
     }
 
